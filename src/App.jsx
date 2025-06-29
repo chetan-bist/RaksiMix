10aec7b259d53f7da1fcad3c72789630cfbc4e89
chetan-bist <bistc3712@gmail.com>
2025-06-29 21:26:34 +0545

    modify app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {ScrollTrigger, SplitText} from 'gsap/all';
+import gsap from 'gsap';
+
+gsap.registerPlugin(ScrollTrigger,SplitText);
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='flex-center h-[100vh]'>
+        <h1 className='text-3xl text-indigo-300 '>hello, GSAP!</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </>
   )
 }
